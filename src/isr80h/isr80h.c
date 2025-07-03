893a524fdbbdf0386a0f881743da46ca7024f5dd
Stephen Planck <splanck9271@gmail.com>
2025-07-03 12:48:05 -0400

    Switch to direct syscall handler

--- src/isr80h/isr80h.c
+++ src/isr80h/isr80h.c
@@ -4,17 +4,23 @@
 #include "heap.h"
 #include "process.h"
 #include "misc.h"
+#include "vana/syscall.h"
 
 void isr80h_register_commands()
 {
-    isr80h_register_command(ISR80H_COMMAND0_SUM, isr80h_command0_sum);
-    isr80h_register_command(ISR80H_COMMAND1_PRINT, isr80h_command1_print);
-    isr80h_register_command(ISR80H_COMMAND2_GETKEY, isr80h_command2_getkey);
-    isr80h_register_command(ISR80H_COMMAND3_PUTCHAR, isr80h_command3_putchar);
-    isr80h_register_command(ISR80H_COMMAND4_MALLOC, isr80h_command4_malloc);
-    isr80h_register_command(ISR80H_COMMAND5_FREE, isr80h_command5_free);
-    isr80h_register_command(ISR80H_COMMAND6_PROCESS_LOAD_START, isr80h_command6_process_load_start);
-    isr80h_register_command(ISR80H_COMMAND7_INVOKE_SYSTEM_COMMAND, isr80h_command7_invoke_system_command);
-    isr80h_register_command(ISR80H_COMMAND8_GET_PROGRAM_ARGUMENTS, isr80h_command8_get_program_arguments);
-    isr80h_register_command(ISR80H_COMMAND9_EXIT, isr80h_command9_exit);
+    /* command table no longer used */
+}
+
+void isr80h_syscall(struct interrupt_frame* context)
+{
+    switch (context->eax)
+    {
+        case VANA_SYS_EXIT:
+            context->eax = (int)isr80h_command9_exit(context);
+            break;
+        /* handle other VANA_SYS_* calls */
+        default:
+            context->eax = VANA_ENOSYS;
+            return;
+    }
 }
