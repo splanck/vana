edfcbbb702760a3978e1929f1988bcf610417ce4
Stephen Planck <splanck9271@gmail.com>
2025-07-02 23:26:19 -0400

    removed unneeded files

--- example_kernel_src/PeachOS-master/programs/stdlib/src/stdio.h
+++
@@ -1,7 +0,0 @@
-#ifndef PEACHOS_STDIO
-#define PEACHOS_STDIO
-
-int putchar(int c);
-int printf(const char *fmt, ...);
-
-#endif
--- example_kernel_src/PeachOS-master/programs/stdlib/src/stdlib.h
+++
@@ -1,8 +0,0 @@
-#ifndef PEACHOS_STDLIB_H
-#define PEACHOS_STDLIB_H
-#include <stddef.h>
-
-void* malloc(size_t size);
-void free(void* ptr);
-char* itoa(int i);
-#endif
